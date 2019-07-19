192d451db32fca14e8ff19e40efa0ec1ba05e2bb
hang <1031948347@qq.com>
2019-07-19 15:01:24 +0800

    11

---
+++ src/main/java/com/feifan/dao/NoticeMapper.java
@@ -0,0 +1,16 @@
+package com.feifan.dao;
+
+import com.feifan.pojo.Notice;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface NoticeMapper {
+
+    //发布
+    public Integer publishNotice(Notice notice);
+    //查询
+    public List<Notice> selectAll();
+
+}
